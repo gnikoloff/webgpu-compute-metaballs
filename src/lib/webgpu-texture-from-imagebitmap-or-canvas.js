4a60f2767d8dc53e8164fbb9217bb5024129274b
Georgi Nikolov <nikoloffgeorgi@gmail.com>
2022-03-13 20:13:04 +0100

    update wglsl syntax

--- src/lib/webgpu-texture-from-imagebitmap-or-canvas.js
+++ src/lib/webgpu-texture-from-imagebitmap-or-canvas.js
@@ -43,23 +43,21 @@ function webGPUGenerateMipmap(gpuDevice, texture, textureDescriptor) {
         vec2<f32>(-1.0, -1.0), vec2<f32>(1.0, -1.0));
 
       struct VertexOutput {
-        [[builtin(position)]] position : vec4<f32>;
-        [[location(0)]] texCoord : vec2<f32>;
+        @builtin(position) position : vec4<f32>;
+        @location(0) texCoord : vec2<f32>;
       };
 
-      [[stage(vertex)]]
-      fn vertexMain([[builtin(vertex_index)]] vertexIndex : u32) -> VertexOutput {
+      @stage(vertex) fn vertexMain(@builtin(vertex_index) vertexIndex : u32) -> VertexOutput {
         var output : VertexOutput;
         output.texCoord = pos[vertexIndex] * vec2<f32>(0.5, -0.5) + vec2<f32>(0.5);
         output.position = vec4<f32>(pos[vertexIndex], 0.0, 1.0);
         return output;
       }
 
-      [[binding(0), group(0)]] var imgSampler : sampler;
-      [[binding(1), group(0)]] var img : texture_2d<f32>;
+      @group(0) @binding(0) var imgSampler : sampler;
+      @group(0) @binding(1) var img : texture_2d<f32>;
 
-      [[stage(fragment)]]
-      fn fragmentMain([[location(0)]] texCoord : vec2<f32>) -> [[location(0)]] vec4<f32> {
+      @stage(fragment) fn fragmentMain(@location(0) texCoord : vec2<f32>) -> @location(0) vec4<f32> {
         return textureSample(img, imgSampler, texCoord);
       }
     `,
