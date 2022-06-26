cf8e81c9cf92088a097947a1211e5ce3eb5cbb6e
gnikoloff <nikoloffgeorgi@gmail.com>
2022-06-26 17:01:13 +0200

    handle base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   assetsInclude: ['**/*.gltf', '**/*.glb', '**/*.bin'],
+  base: process.env.IS_BUILD ? '/webgpu-compute-metaballs/' : '/',
   build: {
     outDir: 'docs',
   },
